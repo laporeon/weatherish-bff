76c6a19dd0cad1d3cde55f3aeec5de5dc02ee0ef
laporeon <34722707+laporeon@users.noreply.github.com>
2023-11-17 15:55:42 -0300

    refactor: remove unused middleware

--- src/server.js
+++ src/server.js
@@ -12,11 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use((req, res, next) => {
-  console.log(`Received request: ${req.method} ${req.url}`);
-  next();
-});
-
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
