27a929423cabffc929f1d72a412e02096b332b0f
laporeon <34722707+laporeon@users.noreply.github.com>
2024-01-02 16:03:21 -0300

    feat: create cron job schedule

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ import 'dotenv/config';
 
 import cors from 'cors';
 import express from 'express';
+import cron from 'node-cron';
 
 import { routes } from './routes/weather-routes.js';
+import { preventRenderSleepMode } from './utils/cronjob.js';
 
 const port = 3000 || process.env.PORT;
 const app = express();
@@ -14,4 +16,9 @@ app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
+});
+
+cron.schedule('*/30 * * * *', () => {
+  console.log('Running every 30 minutes...');
+  preventRenderSleepMode();
 });
